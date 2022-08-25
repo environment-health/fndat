167c79a3c8d8b16a7a5f061a440e1081e3084d8b
David Beauchesne <david.beauchesne@uqar.ca>
2022-08-25 14:00:49 -0400

    add report template and inst folder

---
+++ _pipeline.R
@@ -0,0 +1,16 @@
+library(devtools)
+load_all()
+
+pipeline <- function() {
+  # Update global parameters
+  global_parameters()
+  
+  # Get area of interest 
+  get_aoi()
+  
+  # Make grid 
+  make_grid(cellsize = 0.05)
+
+  # Integrate data 
+  pipedat::pipeflow("./data/data-config/pipedat.yml")
+}
